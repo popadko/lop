3af722d1e48914a9c24239d27adf14797468266b
Oleg Popadko <oleg@popadko.com>
2015-10-15 12:04:53 +0300

    Add delete issue feature

---
+++ src/Luminaire/Bundle/IssueBundle/Controller/Api/Rest/IssueController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Luminaire\Bundle\IssueBundle\Controller\Api\Rest;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+
+/**
+ * @NamePrefix("luminaire_api_")
+ */
+class IssueController extends RestController implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *      description="Delete issue",
+     *      resource=true
+     * )
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function deleteAction($id)
+    {
+        return $this->handleDeleteRequest($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getManager()
+    {
+        return $this->get('luminaire_issue.issue.manager.api');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getForm()
+    {
+        throw new \LogicException('This method should not be called');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormHandler()
+    {
+        throw new \LogicException('This method should not be called');
+    }
+}
