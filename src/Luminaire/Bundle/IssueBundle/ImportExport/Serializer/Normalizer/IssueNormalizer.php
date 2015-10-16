a069a583f724da27047cb8aa2dd95b9b878aa5c4
Oleg Popadko <oleg@popadko.com>
2015-10-16 15:24:03 +0300

    Add issue export

---
+++ src/Luminaire/Bundle/IssueBundle/ImportExport/Serializer/Normalizer/IssueNormalizer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Luminaire\Bundle\IssueBundle\ImportExport\Serializer\Normalizer;
+
+use Luminaire\Bundle\IssueBundle\Entity\Issue;
+use Oro\Bundle\ImportExportBundle\Serializer\Normalizer\ConfigurableEntityNormalizer;
+
+/**
+ * Class IssueNormalizer
+ */
+class IssueNormalizer extends ConfigurableEntityNormalizer
+{
+    /**
+     * @inheritDoc
+     */
+    public function supportsNormalization($data, $format = null, array $context = array())
+    {
+        return $data instanceof Issue;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsDenormalization($data, $type, $format = null, array $context = [])
+    {
+        return is_array($data) && $type == 'Luminaire\Bundle\IssueBundle\Entity\Issue';
+    }
+}
